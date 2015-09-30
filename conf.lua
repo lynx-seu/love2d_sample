9cb8cb564255e5eed147ae3416c88d9f5bb6e038
lynx <seuxl1@163.com>
2015-09-30 22:30:37 +0800

    environment add

--- conf.lua
+++ conf.lua
@@ -1,11 +1,15 @@
+local env = require "environment"
 
 function love.conf(t)
     t.version = "0.9.2"
     t.console = true
 
     t.window.title = "sudoku"
-    t.window.width = 480
-    t.window.height = 800
+    --t.window.width = 480 
+    --t.window.height = 800
+    local size = env.GetWindowSize()
+    t.window.width = size.x
+    t.window.height = size.y
     t.window.resizable = false
 
     t.modules.joystick = false
--- environment.lua
+++ environment.lua
@@ -1,11 +1,50 @@
 local vec2 = require "hump.vector"
 
+local landscape = false
+local screen_size = {
+    iphone_3g = vec2(480, 320),
+    iphone_4 = vec2(960, 640),
+    iphone_5 = vec2(1136, 640),
+    ipad = vec2(1024, 768),
+    ipad_retina = vec2(2048, 1536),
+    android = vec2(800, 480),
+    android_1 = vec2(854, 480),
+    android_2 = vec2(1280, 720),
+    android_3 = vec2(1280, 800),
+    android_4 = vec2(1920, 1080),
+}
+
 local env = {
-    window_size = vec2(480, 800), 
-    res_size = vec2(800, 1280),
-    landscape = false,
+    window_size = screen_size.android,
+    canvas_size = screen_size.android_3,
     res_root = 'assets',
     external_storage_dir = '',
 }
 
+function env.getWindowSize()
+    local size = env.window_size
+    if not landscape then
+        return vec2(size.y, size.x)
+    end
+    return size 
+end
+
+function env.getCanvasSize()
+    local size = env.canvas_size
+    if not landscape then
+        return vec2(size.y, size.x)
+    end
+    return size
+end
+
+function env.getResRoot()
+    return env.res_root
+end
+
+
+return {
+    GetWindowSize = env.getWindowSize,
+    GetCanvasSize = env.getCanvasSize,
+    GetResRoot = env.getResRoot,
+}
 
