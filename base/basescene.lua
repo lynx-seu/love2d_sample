815623842334e4e99ae6803ac668315e313857de
lynx <seuxl1@163.com>
2015-10-15 17:22:39 +0800

    learning tmp

--- base/basescene.lua
+++ base/basescene.lua
@@ -17,11 +17,20 @@ local function transferPos(x, y)
     return x*scale_x, y*scale_y
 end
 
-function Scene:DrawLine(x1, y1, x2, y2)
-    x1, y1 = transferPos(x1, y1)
-    x2, y2 = transferPos(x2, y2)
-    love.graphics.line(x1, y1, x2, y2)
+function Scene:draw()
+    love.graphics.push()
+    love.graphics.scale(_env.GetScale():unpack())
+    self:onDraw()
+    love.graphics.pop()
 end
+
+function Scene:onDraw() end
+
+--function Scene:DrawLine(x1, y1, x2, y2)
+    --x1, y1 = transferPos(x1, y1)
+    --x2, y2 = transferPos(x2, y2)
+    --love.graphics.line(x1, y1, x2, y2)
+--end
 
 
 return setmetatable({new=new}, {__call=function(_, ...) return new(...) end})
--- scenes/game.lua
+++ scenes/game.lua
@@ -1,14 +1,34 @@
 local base = require "base.basescene"
 local game = base()
+local camera = require "hump.camera"
 
-function game:update(dt)
+local lg = love.graphics
+
+local cam
+local t = 0
+local angle = 0
+
+function game:init()
+    cam = camera(400, 640)
 end
 
-function game:draw()
-    love.graphics.print("Game Start", 10, 10)
-    self:DrawLine(0, 0, 400, 640)
-    love.graphics.circle("fill", 400, 640, 50, 6)
-    love.graphics.circle("fill", 240, 400, 50, 6)
+function game:update(dt)
+    print(cam:rotate(math.pi/2 * dt))
+end
+
+--function game:draw()
+    --self:onDraw()
+--end
+
+function game:onDraw()
+    love.graphics.circle("line", 0, 0, 100, 8)
+    cam:draw(function ()
+        love.graphics.circle("line", 400, 640, 100, 8)
+        lg.print("hello,world", 400, 640)
+    end)
+	love.graphics.setPointSize(5)
+	love.graphics.setColor(0, 0, 0xff)
+	love.graphics.point(400, 640)
 end
 
 return game
